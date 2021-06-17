f2e50cddd39ce0fad0ca8cd420508a99e1e6acb9
Egor Kartashov <egor.linkinked@gmail.com>
2021-06-17 12:49:02 +0500

    Добавил на экран срочных задач блок "Сделано", где будут выполненные задачи "из прошлого"

--- app/src/main/java/org/hse/ataskmobileclient/utils/TasksGroupingUtil.kt
+++ app/src/main/java/org/hse/ataskmobileclient/utils/TasksGroupingUtil.kt
@@ -19,6 +19,7 @@ class TasksGroupingUtil {
             val todayTasks : ArrayList<TaskListItem> = arrayListOf()
             val tomorrowTasks : ArrayList<TaskListItem> = arrayListOf()
             val upcomingTasks : ArrayList<TaskListItem> = arrayListOf()
+            val completedTasks: ArrayList<TaskListItem> = arrayListOf()
 
             val (dueToday, dueTomorrow) = getTodayAndTomorrow()
 
@@ -28,7 +29,9 @@ class TasksGroupingUtil {
 
                 val compareToToday = DateTimeComparer.compareDateOnly(task.dueDate, dueToday)
                 if (compareToToday < 0) {
-                    if (!task.isCompleted)
+                    if (task.isCompleted)
+                        completedTasks.add(task)
+                    else
                         overdueTasks.add(task)
                     continue
                 }
@@ -47,15 +50,17 @@ class TasksGroupingUtil {
 
 
             val sdf = SimpleDateFormat("dd MMMM", Locale.getDefault())
-            val overdueStringHeader = context.getString(R.string.overdue_tasks_header)
-            val todayStringHeader = context.getString(R.string.today_tasks_header)
-            val tomorrowStringHeader = context.getString(R.string.tomorrow_tasks_header)
-            val upcomingStringHeader = context.getString(R.string.upcoming_tasks_header)
+            val overdueStringHeaderString = context.getString(R.string.overdue_tasks_header)
+            val todayStringHeaderString = context.getString(R.string.today_tasks_header)
+            val tomorrowStringHeaderString = context.getString(R.string.tomorrow_tasks_header)
+            val upcomingStringHeaderString = context.getString(R.string.upcoming_tasks_header)
+            val completedDeadlineTasksHeaderString = context.getString(R.string.deadline_completed_tasks_header)
 
-            val overdueHeader = TasksHeader(overdueStringHeader)
-            val todayHeader = TasksHeader("${todayStringHeader}, ${sdf.format(dueToday)}")
-            val tomorrowHeader = TasksHeader("${tomorrowStringHeader}, ${sdf.format(dueTomorrow)}")
-            val upcomingHeader = TasksHeader(upcomingStringHeader)
+            val overdueHeader = TasksHeader(overdueStringHeaderString)
+            val todayHeader = TasksHeader("${todayStringHeaderString}, ${sdf.format(dueToday)}")
+            val tomorrowHeader = TasksHeader("${tomorrowStringHeaderString}, ${sdf.format(dueTomorrow)}")
+            val upcomingHeader = TasksHeader(upcomingStringHeaderString)
+            val completedTasksHeader = TasksHeader(completedDeadlineTasksHeaderString)
 
             val groupedDeadlineTasks : ArrayList<TaskListItem> = arrayListOf()
             if (overdueTasks.any()) {
@@ -76,6 +81,11 @@ class TasksGroupingUtil {
             if (upcomingTasks.any()) {
                 groupedDeadlineTasks.add(upcomingHeader)
                 groupedDeadlineTasks.addAll(upcomingTasks)
+            }
+
+            if (completedTasks.any()) {
+                groupedDeadlineTasks.add(completedTasksHeader)
+                groupedDeadlineTasks.addAll(completedTasks)
             }
 
             if (!groupedDeadlineTasks.any()) {
